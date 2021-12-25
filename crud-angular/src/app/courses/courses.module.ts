a420a899139cdcc8ba9f716b0a5257ab1679972b
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2021-12-25 20:01:30 -0300

    Refatorando e adiconando novos componentes

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
@@ -10,10 +10,11 @@ import { CoursesComponent } from './courses/courses.component';
   declarations: [
     CoursesComponent
   ],
+
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule,
-  ]
+    AppMaterialModule
+  ],
 })
 export class CoursesModule { }
