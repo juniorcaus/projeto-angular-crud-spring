df8446cfa5a5854dcf64a245c8e2869be8c3815c
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2021-12-25 17:07:03 -0300

    Adicionando tabelas e módulos do Angular material

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
