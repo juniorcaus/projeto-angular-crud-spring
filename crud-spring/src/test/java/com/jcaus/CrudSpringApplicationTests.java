253883f7da94ad4290a26b17c05ec9308dadce6a
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2021-12-31 00:51:46 -0300

    hello word com Spring

---
+++ crud-spring/src/test/java/com/jcaus/CrudSpringApplicationTests.java
@@ -0,0 +1,18 @@
+package com.jcaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
